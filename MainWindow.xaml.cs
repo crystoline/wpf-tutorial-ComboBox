ebe7c67ad96223679993ef7422b4db5abf439ebb
crystoline <crystoline@gmail.com and git remote add origin https://github.com/crystoline/wpf-tutorial-ComboBox.git>
2018-12-02 13:07:45 +0100

    Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace WpfCombobox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+      
+            Colors.ItemsSource = typeof (Colors).GetProperties();
+        }
+
+    }
+
+    
+}
